3d73caa5be117812b72cc690f7956134f868bcb5
ducson20 <leson2620@gmail.com>
2021-05-23 21:43:31 +0700

    first commit

---
+++ eclipse-workspace/JsonWebToken/src/main/java/web/jwt/JsonWebTokenApplication.java
@@ -0,0 +1,13 @@
+package web.jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonWebTokenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonWebTokenApplication.class, args);
+	}
+
+}
---
+++ eclipse-workspace/JsonWebToken/src/test/java/web/jwt/JsonWebTokenApplicationTests.java
@@ -0,0 +1,13 @@
+package web.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonWebTokenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
